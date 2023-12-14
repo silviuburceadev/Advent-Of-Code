c350828ef62ebbfcb480beb31908ada0a61bf74e
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-14 09:44:58 +0200

    Day 1 Test 1 - TDD RED
    - add failing test to parse the 2 digits inside the word

--- Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/TestCalibration.java
+++ Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/TestCalibration.java
@@ -4,12 +4,19 @@ import com.github.silviuburceadev.aoc.Calibration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestCalibration {
 
     private static final Calibration INSTANCE = new Calibration();
 
     @Test
     public void testParseLeadTailDigit() {
-        Assertions.assertEquals(12, INSTANCE.parse("1abc2"));
+        assertEquals(12, INSTANCE.parse("1abc2"));
+    }
+    
+    @Test
+    public void testParseTwoDigitsInside() {
+        assertEquals(38, INSTANCE.parse("pqr3stu8vwx"));
     }
 }
