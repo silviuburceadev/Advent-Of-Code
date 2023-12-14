2e73a2be73db8c4d6fc68102bd6b609efa91d9fe
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-14 13:47:20 +0200

    Day 1 Part 2 - TDD RED
    - Added more test cases to verify the parsing order

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/LetterCalibration.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/LetterCalibration.java
@@ -3,11 +3,15 @@ package com.github.silviuburceadev.aoc;
 public class LetterCalibration extends Calibration {
 
     /**
-     * Digit replacements for their letter counterpart. Note that 2 and 3 must come after 8, because
-     * <code>eightwo</code> must become <code>8wo</code>, not <code>eigh2</code> to preserve left-to-right parsing.
+     * Digit replacements for their letter counterpart. Note that the digits are not in their natural order, to
+     * preserve the left-to-right parsing.
+     * - 8 precedes 2 and 3, so <code>eightwo</code> becomes <code>8wo</code>, not <code>eigh2</code>
+     * - 1, 5 and 9 must precede 8, so <code>fiveight</code> becomes <code>5ight</code>
+     * - 7 must precede 9, so <code>sevenine</code> becomes <code>7ine</code>
+     * - 2 must precede 1, so <code>twone</code> becomes <code>2ne</code>
      */
     enum Digit {
-        ONE("1"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"), TWO("2"), THREE("3");
+        ONE("1"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), NINE("9"), EIGHT("8"), TWO("2"), THREE("3");
 
         public final String value;
 
--- Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/TestLetterCalibration.java
+++ Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/TestLetterCalibration.java
@@ -12,4 +12,22 @@ public class TestLetterCalibration {
     public void testParseLeadTailLetters() {
         assertEquals(29, INSTANCE.parse("two1nine"));
     }
+
+    @Test
+    public void testParsingOrder() {
+        // 8 before 2
+        assertEquals(83, INSTANCE.parse("eightwothree"));
+        // 2 before 1
+        assertEquals(24, INSTANCE.parse("xtwone3four"));
+        // 1 before 8
+        assertEquals(14, INSTANCE.parse("zoneight234"));
+    }
+
+    @Test
+    public void testParseLettersInside() {
+        assertEquals(13, INSTANCE.parse("abcone2threexyz"));
+        assertEquals(42, INSTANCE.parse("4nineeightseven2"));
+        assertEquals(76, INSTANCE.parse("7pqrstsixteen"));
+    }
+
 }
