3d467334c1a35e8be745c294bee3fc44a67564ed
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-14 09:32:36 +0200

    Day 1 Part 1 - TDD GREEN
    - added implementation for test case where digits are first/last

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/Calibration.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/Calibration.java
@@ -3,6 +3,6 @@ package com.github.silviuburceadev.aoc;
 public class Calibration {
 
     public int parse(String input) {
-        return 0;
+        return (input.charAt(0) - '0') * 10 + (input.charAt(input.length() - 1) - '0');
     }
 }
