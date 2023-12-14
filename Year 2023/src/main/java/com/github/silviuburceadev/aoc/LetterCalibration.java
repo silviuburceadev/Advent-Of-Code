81ca18cfe2d2427e71a1b9c7715ed9c4bf5eac5e
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-14 13:12:59 +0200

    Day 1 Part 2 - TDD REFACTOR
    - extracted logic to parse all the digits

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/LetterCalibration.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/LetterCalibration.java
@@ -2,13 +2,30 @@ package com.github.silviuburceadev.aoc;
 
 public class LetterCalibration extends Calibration {
 
+    /**
+     * Digit replacements for their letter counterpart. Note that 2 and 3 must come after 8, because
+     * <code>eightwo</code> must become <code>8wo</code>, not <code>eigh2</code> to preserve left-to-right parsing.
+     */
+    enum Digit {
+        ONE("1"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"), TWO("2"), THREE("3");
+
+        public final String value;
+
+        Digit(String value) {
+            this.value = value;
+        }
+    }
+
     @Override
     public int parse(String input) {
         final String filtered = filter(input);
         return super.parse(filtered);
     }
 
-    private String filter(final String input) {
-        return input.replace("two", "2").replace("nine", "9");
+    private String filter(String input) {
+        for (Digit digit : Digit.values()) {
+            input = input.replace(digit.name().toLowerCase(), digit.value);
+        }
+        return input;
     }
 }
