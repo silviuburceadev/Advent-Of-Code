a50d5c0fae5ad9fbd298c161106d60db5b6bd5dd
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-19 18:07:56 +0200

    Day 4 Part 1 - TDD GREEN
    - added implementation for parsing a game

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/scratchcard/Game.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/scratchcard/Game.java
@@ -1,7 +1,12 @@
 package com.github.silviuburceadev.aoc.scratchcard;
 
-public class Game {
+import static java.util.Arrays.stream;
+
+public record Game(Card winningCard, Card playingCard) {
     public static Game parse(String input) {
-        return null;
+        String[] cards = input.substring(input.indexOf(":") + 1).split("\\|");
+        final Card winningCard = new Card(stream(cards[0].trim().split("\\D+")).map(Integer::valueOf).toList());
+        final Card playingCard = new Card(stream(cards[0].trim().split("\\D+")).map(Integer::valueOf).toList());
+        return new Game(winningCard, playingCard);
     }
 }
