3bc9d447c9af94e3da50cbef30583171756a1412
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-19 16:17:30 +0200

    Day 4 Part 1 - TDD GREEN
    - added implementation for parsing the card

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/scratchcard/Card.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/scratchcard/Card.java
@@ -2,8 +2,14 @@ package com.github.silviuburceadev.aoc.scratchcard;
 
 import java.util.List;
 
+import static java.util.Arrays.stream;
+
 public record Card(List<Integer> numbers) {
-    public static Card parse(String s) {
-        return null;
+    public static Card parse(String input) {
+        // +/- 2 to skip the target character and the space next to it
+        List<Integer> numbers = stream(input.substring(input.indexOf(":") + 2, input.indexOf("|") - 2).split("\\D+"))
+                .map(Integer::parseInt)
+                .toList();
+        return new Card(numbers);
     }
 }
