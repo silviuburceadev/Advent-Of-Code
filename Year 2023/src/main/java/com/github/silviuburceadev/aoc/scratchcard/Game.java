6d1353afad10c3bc975b11952ab004ba58684e74
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-19 17:56:59 +0200

    Day 4 Part 1 - TDD RED
    - add failing test for parsing a game

---
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/scratchcard/Game.java
@@ -0,0 +1,7 @@
+package com.github.silviuburceadev.aoc.scratchcard;
+
+public class Game {
+    public static Game parse(String input) {
+        return null;
+    }
+}
--- Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/scratchcard/TestCard.java
+++ Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/scratchcard/TestCard.java
@@ -1,13 +1,19 @@
 package com.github.silviuburceadev.aoc.test.scratchcard;
 
 import com.github.silviuburceadev.aoc.scratchcard.Card;
+import com.github.silviuburceadev.aoc.scratchcard.Game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCard {
 
     @Test
-    public void testCard() {
+    public void testParsingCard() {
         Assertions.assertNotNull(Card.parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"));
+    }
+
+    @Test
+    public void testParsingGame() {
+        Assertions.assertNotNull(Game.parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"));
     }
 }
