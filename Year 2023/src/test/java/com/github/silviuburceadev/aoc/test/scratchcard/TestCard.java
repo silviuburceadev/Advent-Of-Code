719b71f90e8c02a5ac8b77ff82bd4f20b9a5deab
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-20 17:40:23 +0200

    Day 4 Part 2 - TDD REFACTOR
    - test clean-up

--- Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/scratchcard/TestCard.java
+++ Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/scratchcard/TestCard.java
@@ -14,15 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestCard {
 
-    private static final String[] SAMPLE = """
-        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-        """.split(System.lineSeparator());
-
     @Test
     public void testParsingGame() {
         final Game game = Game.parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
@@ -60,7 +51,7 @@ public class TestCard {
 
     @Test
     public void testMatches() {
-        final Game game = Game.parse(SAMPLE[0]);
+        final Game game = Game.parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
         assertEquals(4, game.getMatches());
     }
 }
