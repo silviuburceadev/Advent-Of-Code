c0c19dd2dd5eb946fa06eeedeab6c66dcbde1cc3
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-21 16:38:09 +0200

    Day 5 Part 1 - TDD GREEN
    - add implementation for parsing sections

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/garden/Garden.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/garden/Garden.java
@@ -8,11 +8,30 @@ import static java.util.Arrays.stream;
 public record Garden(List<Integer> seeds, List<SectionRange> sections) {
     public static Garden parse(String[] input) {
         final String seeds = input[0].substring("seeds: ".length());
+        String name = null;
+        List<SectionRange> sections = new ArrayList<>();
+        List<DestSrcRange> ranges = new ArrayList<>();
+
+        for (int i = 2; i < input.length; i++) {
+            final String line = input[i].trim();
+            if (line.endsWith("map:")) {
+                name = line.replaceAll("\\s+map:$", "");
+            } else if (!line.isBlank()) {
+                ranges.add(new DestSrcRange(line));
+            } else {
+                // empty line, section end
+                sections.add(new SectionRange(name, ranges));
+                // reset ranges
+                ranges = new ArrayList<>();
+            }
+        }
+        // add last section
+        sections.add(new SectionRange(name, ranges));
+
         return new Garden(
             stream(seeds.split("\\D+")).map(Integer::valueOf).toList(),
-            new ArrayList<>()
+            sections
         );
-
     }
 
     public List<Integer> getSeeds() {
