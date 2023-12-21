bc10db02b3a62d3d28e099cadc2e455ea4c15b0e
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-21 13:25:39 +0200

    Day 5 Part 2 - TDD GREEN
    - add implementation for parsing the seeds

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/garden/Garden.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/garden/Garden.java
@@ -1,18 +1,16 @@
 package com.github.silviuburceadev.aoc.garden;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public record Garden(List<Integer> seeds) {
     public static Garden parse(String[] input) {
         final String seeds = input[0].substring("seeds: ".length());
-        System.out.println(seeds);
         return new Garden(Arrays.stream(seeds.split("\\D+")).map(Integer::valueOf).toList());
 
     }
 
     public List<Integer> getSeeds() {
-        return new ArrayList<>();
+        return seeds;
     }
 }
