e030984c913488db3d5939e03867637f5963713e
Silviu BURCEA <silviuburceadev@gmail.com>
2023-12-29 12:32:48 +0200

    Day 6 Part 1 - TDD GREEN
    - added implementation to count number of ways to win

--- Year 2023/src/main/java/com/github/silviuburceadev/aoc/race/Race.java
+++ Year 2023/src/main/java/com/github/silviuburceadev/aoc/race/Race.java
@@ -2,6 +2,15 @@ package com.github.silviuburceadev.aoc.race;
 
 public record Race(int time, int distance) {
     public int waysToWin() {
-        return 0;
+        int waysToWin = 0;
+        for (int i = time / 2; i > 0; i--) {
+            if (i * (time - i) >= distance) {
+                // we count twice, because holding for i seconds and going for (time - i) seconds is the same
+                // as holding for (time - i) seconds and going for i seconds
+                waysToWin += 2;
+            }
+        }
+        if (time % 2 == 0) waysToWin++;
+        return waysToWin;
     }
 }
