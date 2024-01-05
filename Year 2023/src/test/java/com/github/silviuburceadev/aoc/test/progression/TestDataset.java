ff1a9e881121a49f970c0b9a65d4e13ac533ec79
Silviu BURCEA <silviuburceadev@gmail.com>
2024-01-05 18:34:32 +0200

    Day 9 Part 2 - TDD REFACTOR
    - added test for main input

--- Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/progression/TestDataset.java
+++ Year 2023/src/test/java/com/github/silviuburceadev/aoc/test/progression/TestDataset.java
@@ -8,9 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.teeing;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDataset {
@@ -56,12 +59,32 @@ public class TestDataset {
     }
 
     @Test
+    public void testSamplePrevious() {
+        final List<Dataset> datasets = asList(
+                new Dataset(0, 3, 6, 9, 12, 15),
+                new Dataset(1, 3, 6, 10, 15, 21),
+                new Dataset(10, 13, 16, 21, 30, 45)
+        );
+        assertEquals(2, datasets.stream().mapToInt(Dataset::getPreviousValue).sum());
+    }
+
+    @Test
     public void testMainInput() throws IOException {
         try (InputStream resource = TestDataset.class.getResourceAsStream("day9.in")) {
             assert resource != null;
             try (InputStreamReader inputStreamReader = new InputStreamReader(resource, StandardCharsets.UTF_8);
                  BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
-                assertEquals(2105961943, bufferedReader.lines().map(Dataset::new).mapToInt(Dataset::getNextValue).sum());
+                final List<Integer> prevNext = bufferedReader.lines().map(Dataset::new)
+                        .collect(
+                            teeing(
+                                summingInt(Dataset::getPreviousValue),
+                                summingInt(Dataset::getNextValue),
+                                Arrays::asList
+                            )
+                        );
+                assertEquals(2, prevNext.size());
+                assertEquals(1019, prevNext.get(0));
+                assertEquals(2105961943, prevNext.get(1));
             }
         }
     }
